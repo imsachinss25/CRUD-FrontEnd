0b1ab0dedb1014b834aa1501ecfbcf1f040966ba
imsachinss25 <63443790+imsachinss25@users.noreply.github.com>
2021-04-21 13:47:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
+import 'antd/dist/antd.css'
 
 ReactDOM.render(
   <BrowserRouter>
