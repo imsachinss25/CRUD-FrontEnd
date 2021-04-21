17efd0a40538b6d87af220ff19253a7fdaa28894
imsachinss25 <63443790+imsachinss25@users.noreply.github.com>
2021-04-21 21:57:54 +0530

    Update Display.jsx

--- src/pages/component/hoc/display/Display.jsx
+++ src/pages/component/hoc/display/Display.jsx
@@ -1,73 +1,94 @@
 import React, { Component, Fragment , useState, useEffect} from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import EditIcon from '@material-ui/icons/Edit';
-import {EditOutlined} from '@ant-design/icons';
+import { Table, Tag, Space } from 'antd';
+import { Form, Input, Button, Radio } from 'antd';
+import { useHistory } from 'react-router-dom';
+import getStudents from '../services/student';
+
 const Display = ()=> {
+
+    let history = useHistory();
+    const columns = [
+        {
+          title: 'Name',
+          dataIndex: 'Name',
+          key: 'Name',
+          
+        },
+        {
+          title: 'LastName',
+          dataIndex: 'LastName',
+          key: 'LastName',
+        },
+        {
+          title: 'Phone',
+          dataIndex: 'Phone',
+          key: 'Phone',
+        },
+        {
+            title: 'Roll',
+            dataIndex: 'Roll',
+            key: 'Roll',
+          },
+          {
+            title: 'Address',
+            dataIndex: 'Address',
+            key: 'Address',
+          },
+          
+          {
+            title: ' Action ',
+            key: '_id',
+            dataIndex: '_id',
+            render: (text) => (
+                <span>
+                  <a onClick={() => handleEditBtnClick(text)}>
+                    <Button> edit </Button>
+                  </a>
+                </span>
+              ),       
+               
+          },
+      ];
+
+      const handleEditBtnClick =(index)=>{
+          data.map((value)=>{
+              if(value._id==index)
+              {
+                  
+                history.push({
+                    pathname: "/Display/"+value._id,
+                state: [{Name: value.Name,
+                LastName: value.LastName,
+                Phone: value.Phone,
+                Roll: value.Roll,
+                Address: value.Address,
+                Key: value._id }]
+
+                });
+              }
+          })
+      }
+    
+
+
+    
     const [data,setData]=useState([]) ;
     useEffect(()=>{ 
-        var request = new XMLHttpRequest(); 
-        request.onreadystatechange = function() { 
-        if (request.status == 200) { 
-        const response=JSON.parse(request.response) 
-        setData(response) 
-        } 
-        }; 
-        request.open('GET', 'http://localhost:8000/students', true); 
-        request.send(); 
+        fetch("http://localhost:8000/students")
+      .then((response) => response.json())
+      .then((data) => setData(data) );
         },[]) 
         
-        useEffect(()=>{ 
-        console.log(data[0]) 
-        },[]) 
+    
 
+        
    return(
 
  <Fragment>
-<div className="display">
-<h2> Students Details </h2>
 
-{
- 
-                data.map((value)=>{
-                    return (
-                        <Fragment>
-                        
-                        <div>
-                        <div className="showDataStyle" > 
-                        <div className=" dataItem heading">
-                               <li> Name </li> 
-                               <li> LastName </li> 
-                               <li> Phone </li> 
-                               <li> Roll </li> 
-                               <li> Address </li>  
-                       </div>
-                       
-                       <div className="dataItem items">
-                               <li> {value.Name} </li> 
-                               <li>{value.LastName} </li> 
-                               <li> {value.Phone} </li> 
-                               <li>{value.Roll}</li> 
-                               <li> {value.Address}</li> 
-                       </div>
-                  
-                                   <Link to={{
-                                       pathname: "/Display/"+value._id,
-                                       state: [{Name: value.Name,
-                                       LastName: value.LastName,
-                                       Phone: value.Phone,
-                                       Roll: value.Roll,
-                                       Address: value.Address,
-                                       Key: value._id }]
-                                   }}>
-                             <EditOutlined  className="btn" /></Link>
-                       </div>
-                       </div> 
-                       </Fragment>
-                    ) 
-                })
-           }
+<h1> Students Data </h1>
 
-</div>
+<Table columns={columns} dataSource={data}/>
 
 </Fragment>
                         
